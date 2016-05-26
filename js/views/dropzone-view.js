a0d43093beeda47720fc61c049c59029e6f364d1
Konstantin Baierer <unixprog@gmail.com>
2016-05-26 19:21:05 +0200

    Dropzone view: hide on 'app:loading'

--- js/views/dropzone-view.js
+++ js/views/dropzone-view.js
@@ -1,2 +1,8 @@
-function Dropzone() {
+function Dropzone(opts) {
+    for (var key in opts) { this[key] = opts[key]; }
+    this.$el = $(this.el);
 }
+Dropzone.prototype.render = function() {
+    var self = this;
+    window.app.on('app:loading', function hideDropzone() { self.$el.addClass('hidden'); });
+};
