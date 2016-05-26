5e77fec3368421ea97f3e149954c43158c59dc49
Konstantin Baierer <unixprog@gmail.com>
2016-05-26 19:19:13 +0200

    Page model: add save and proper line models
    *  save implemented
    * toJSON: Reduces information back to server-/template-friendly format
    * bind 'changed' state to 'app:changed'/'app:saved' events

--- js/model/page.js
+++ js/model/page.js
@@ -1,4 +1,5 @@
 function Page(urlOrOpts) {
+    this.lines = [];
     if (typeof urlOrOpts === 'string') {
         this.imageUrl = urlOrOpts;
     } else {
@@ -6,19 +7,52 @@ function Page(urlOrOpts) {
         for (key in urlOrOpts) { this[key] = urlOrOpts[key]; }
     }
     this.changed = false;
+    window.app.on('app:changed', function setChanged() { self.changed = true; });
+    window.app.on('app:saved', function setUnChanged() { self.changed = false; });
 }
 
-Page.prototype.save = function(cb) {
+Page.prototype.toJSON = function() {
+    var ret = {
+        'line-comments': [],
+        'line-transcriptions': [],
+        'page-comment': this['page-comment'],
+        'ids': this.ids,
+        'url': this.url,
+    };
+    for (var i = 0; i < this.lines.length ; i++) {
+        ret['line-comments'][i] = this.lines[i].comment.trim();
+        ret['line-transcriptions'][i] = this.lines[i].transcription.trim();
+    }
+    return ret;
+};
+
+Page.prototype.save = function savePage(cb) {
+    $.ajax({
+        type: 'POST',
+        url: 'ocr-gt-tools.cgi?action=save',
+        contentType: 'application/json; charset=UTF-8',
+        data: JSON.stringify(this.toJSON()),
+        success: function() { cb(); },
+        error: cb
+    });
 };
 
 Page.prototype.load = function(cb) {
     var self = this;
     $.ajax({
         type: 'GET',
-        url: 'ocr-gt-tools.cgi?action=create&imageUrl=' + this.imageUrl,
+        url: 'ocr-gt-tools.cgi?action=get&imageUrl=' + this.imageUrl,
         error: cb,
         success: function(res) {
             for (key in res) { self[key] = res[key]; }
+            for (var i = 0; i < self['line-transcriptions'].length; i++)  {
+                self.lines.push(new Line({
+                    id: i,
+                    transcription: self['line-transcriptions'][i],
+                    comment: self['line-comments'][i],
+                    image: self['line-images'][i],
+                }));
+            }
             cb();
         },
     });
