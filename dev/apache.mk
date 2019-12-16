f4978c3c14995ee8bf3ff59a1c4f64f7c9a3b389
Stefan Weil <sw@weilnetz.de>
2019-12-16 11:37:33 +0100

    Simplify setting of permissions and owner for Apache2 installation
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- dev/apache.mk
+++ dev/apache.mk
@@ -13,5 +13,5 @@ SUDO_APACHE = sudo -u $(APACHE_USER)
 deploy:
 	$(SUDO_APACHE) mkdir -p "$(APACHE_DIR)/$(APACHE_BASEURL)"
 	$(SUDO_APACHE) cp -r dist/* dist/.htaccess "$(APACHE_DIR)/$(APACHE_BASEURL)"
-	$(SUDO_APACHE) find "$(APACHE_DIR)/$(APACHE_BASEURL)" -exec chmod u+w -R {} \;
-	$(SUDO_APACHE) find "$(APACHE_DIR)/$(APACHE_BASEURL)" -exec chown $(APACHE_USER):$(APACHE_GROUP) {} \;
+	$(SUDO_APACHE) chmod -R u+w "$(APACHE_DIR)/$(APACHE_BASEURL)"
+	$(SUDO_APACHE) chown -R $(APACHE_USER):$(APACHE_GROUP) "$(APACHE_DIR)/$(APACHE_BASEURL)"
