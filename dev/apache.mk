3e0f812b6eac1726106b686a52d89bcde766165d
Stefan Weil <sw@weil.de>
2020-12-16 14:53:37 +0100

    Fix installation to /var/www/html
    It did not work if that directory was owned by root (Debian default).
    
    Signed-off-by: Stefan Weil <sw@weil.de>

--- dev/apache.mk
+++ dev/apache.mk
@@ -4,14 +4,14 @@ APACHE_USER = www-data
 APACHE_GROUP = www-data
 APACHE_DIR = /var/www/html
 APACHE_BASEURL = ocr-gt
-SUDO_APACHE = sudo -u $(APACHE_USER)
+SUDO = sudo
 
 #
 # Deploy on apache
 #
 
 deploy:
-	$(SUDO_APACHE) mkdir -p "$(APACHE_DIR)/$(APACHE_BASEURL)"
-	$(SUDO_APACHE) chown -R $(APACHE_USER):$(APACHE_GROUP) "$(APACHE_DIR)/$(APACHE_BASEURL)"
-	$(SUDO_APACHE) chmod -R u+w "$(APACHE_DIR)/$(APACHE_BASEURL)"
-	$(SUDO_APACHE) cp -r dist/* dist/.htaccess "$(APACHE_DIR)/$(APACHE_BASEURL)"
+	$(SUDO) mkdir -p "$(APACHE_DIR)/$(APACHE_BASEURL)"
+	$(SUDO) cp -r dist/* dist/.htaccess "$(APACHE_DIR)/$(APACHE_BASEURL)"
+	$(SUDO) chown -R $(APACHE_USER):$(APACHE_GROUP) "$(APACHE_DIR)/$(APACHE_BASEURL)"
+	$(SUDO) chmod -R u+w "$(APACHE_DIR)/$(APACHE_BASEURL)"
